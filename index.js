890d3fccae79349b26a140be8aa44ceba0576dbf
lmkirkiii@gmail.com Kirk <lmkirkiii@gmail.com>
2018-05-10 18:20:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,14 @@ function johnLennonFacts(facts){
   }
   return emptyArray
 }
+
+function iLoveTheBeatles(n){
+  var emptyArray = [];
+  do{
+    emptyArray.push("I love the Beatles");
+    n++;
+  }
+  while(n<15)
+
+  return emptyArray
+}
